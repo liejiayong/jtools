6e274ddaa0527cf9799fbadf83aa5fd0845eeaa3
liejiayong <809206619@qq.com>
2021-06-02 16:25:37 +0800

    立即执行函数写法

---
+++ iife.js
@@ -0,0 +1,28 @@
+/**
+ * @description: 立即执行函数写法
+ * @author: liejiayong(809206619@qq.com)
+ * @Date: 2021-06-02 16:24:49
+ */
+(function () {
+  /* code */
+})();
+
+(function () {
+  /* code */
+})();
+
+!(function () {
+  /* code */
+})();
+
+~(function () {
+  /* code */
+})();
+
+-(function () {
+  /* code */
+})();
+
++(function () {
+  /* code */
+})();
